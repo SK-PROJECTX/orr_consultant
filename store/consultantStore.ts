6faa726dd0c4b840c68e1b7ebc4bf4a6ddb1e504
SK-PROJECTX <skprojectx12@gmail.com>
2026-07-09 16:10:33 +0100

    fixed build error

--- store/consultantStore.ts
+++ store/consultantStore.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
+import { OpportunityStage } from '@/components/tabs/OpportunitiesTab';
 
 // Types
 export interface ProfileData {
