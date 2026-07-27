b2f8fac74cdb1442feda526c653e3d7bd76f86a4
Abdullateef2006 <ojugbelelateef2006@gmail.com>
2026-07-27 07:25:37 +0100

    fix: hardcode google client id in next.config.ts to ensure it's available in production builds

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   trailingSlash: false,
+  env: {
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: '105825824472-kkg988lc04agqu206kkmt92iplcq0lvh.apps.googleusercontent.com',
+  },
   async headers() {
     return [
       {
