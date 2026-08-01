e68026791e3a659570540d8e5b424f9821febf72
SK-PROJECTX <skprojectx12@gmail.com>
2026-08-01 02:47:42 +0100

    feat: configure next.js output to static export

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
+    output: 'export',
     trailingSlash: false,
     env: {
         NEXT_PUBLIC_GOOGLE_CLIENT_ID: '105825824472-kkg988lc04agqu206kkmt92iplcq0lvh.apps.googleusercontent.com',
